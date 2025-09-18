cc72a5131d5aa8acf1b6fb2dd50325df2492144c
alex <bondareko.alex.sergeevich@gmail.com>
2025-09-18 21:50:14 +0200

    Feature: solve the tasks, get all needed values
            'race_count': race_count,
            'average_age_men': average_age_men,
            'percentage_bachelors': percentage_bachelors,
            'higher_education_rich': higher_education_rich,
            'lower_education_rich': lower_education_rich,
            'min_work_hours': min_work_hours,
            'rich_percentage': rich_percentage,
            'highest_earning_country': highest_earning_country,
            'highest_earning_country_percentage':
            highest_earning_country_percentage,
            'top_IN_occupation': top_IN_occupation

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -3,42 +3,52 @@ import pandas as pd
 
 def calculate_demographic_data(print_data=True):
     # Read data from file
-    df = None
+    df = pd.read_csv('adult.data.csv')
+
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
-    race_count = None
+    race_count = df['race'].value_counts()
 
     # What is the average age of men?
-    average_age_men = None
+    average_age_of_men_mask = df['sex'] == 'Male'
+    average_age_men = df[average_age_of_men_mask]['age'].mean().round(1)
 
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = None
+    bachelors_mask = df['education'] == 'Bachelors'
+    percentage_bachelors = (bachelors_mask.mean() * 100).round(1)
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
     # What percentage of people without advanced education make more than 50K?
 
     # with and without `Bachelors`, `Masters`, or `Doctorate`
-    higher_education = None
-    lower_education = None
+    advanced_edu_mask = ((df['education'] == 'Bachelors')
+                         | (df['education'] == 'Masters')
+                         | (df['education'] == 'Doctorate'))
+    lower_edu_mask = ((df['education'] != 'Bachelors')
+                      & (df['education'] != 'Masters')
+                      & (df['education'] != 'Doctorate'))
+    salary_mask = df['salary'] == ">50K"
 
     # percentage with salary >50K
-    higher_education_rich = None
-    lower_education_rich = None
+    higher_education_rich = ((salary_mask & advanced_edu_mask).sum() / advanced_edu_mask.sum() * 100).round(1)
+    lower_education_rich = ((salary_mask & lower_edu_mask).sum() / lower_edu_mask.sum() * 100).round(1)
 
     # What is the minimum number of hours a person works per week (hours-per-week feature)?
-    min_work_hours = None
+    min_work_hours = df['hours-per-week'].min()
 
     # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
-    num_min_workers = None
+    min_hours_mask = min_work_hours == df['hours-per-week']
 
-    rich_percentage = None
+    rich_percentage = (min_hours_mask & salary_mask).sum() / min_hours_mask.sum() * 100
 
     # What country has the highest percentage of people that earn >50K?
-    highest_earning_country = None
-    highest_earning_country_percentage = None
+    countries_total = df['native-country'].value_counts()
+    highest_earning_country = (df[salary_mask]['native-country'].value_counts() / countries_total * 100).sort_values(ascending=False).index[0]
+    highest_earning_country_percentage = (df[salary_mask]['native-country'].value_counts() / countries_total * 100).sort_values(ascending=False).values[0].round(1)
 
     # Identify the most popular occupation for those who earn >50K in India.
-    top_IN_occupation = None
+    country_india_mask = df['native-country'] == 'India'
+    top_IN_occupation = df[salary_mask & country_india_mask]['occupation'].value_counts().sort_values(ascending=False).index[0]
 
     # DO NOT MODIFY BELOW THIS LINE
 
@@ -67,3 +77,4 @@ def calculate_demographic_data(print_data=True):
         highest_earning_country_percentage,
         'top_IN_occupation': top_IN_occupation
     }
+calculate_demographic_data()
